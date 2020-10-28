9b5ac398aa8240e3f097636383e6bcd7b6a071a2
Alessandro Benedetti <a.benedetti@sease.io>
2020-10-28 12:05:14 +0000

    Initial commit

---
+++ main_v2.py
@@ -0,0 +1,231 @@
+import time
+import utils
+import numpy as np
+import pandas as pd
+
+def create_primary_dataset(adore_beauty_dataset):
+    primary_data = adore_beauty_dataset.copy()
+    primary_data['click_per_query_primary'] = adore_beauty_dataset['click_per_query'].max()
+    primary_data['new_interactions_to_add'] = primary_data['click_per_query_primary'] - primary_data['click_per_query']
+    data_to_add = primary_data.drop_duplicates(
+        subset=['queryId', 'new_interactions_to_add'], keep='last')[['queryId', 'new_interactions_to_add',
+                                                                     'click_per_query']].reset_index(drop=True)
+    primary_data.drop(columns=['click_per_query', 'new_interactions_to_add'], inplace=True)
+    primary_data.rename(columns={'click_per_query_primary': 'click_per_query'}, inplace=True)
+
+    new_data = utils.generate_new_data(data_to_add, adore_beauty_dataset['userId'].max())
+    primary_data = primary_data.append(new_data, ignore_index=True)
+
+    primary_data = primary_data[['userId', 'click_per_userId', 'queryId', 'click_per_query', 'click_per_model_A']]
+
+    print('Nan stats:')
+    print(primary_data.isnull().sum())
+
+    return primary_data
+
+
+def create_variation_dataset(primary_data, click_per_query_adore):
+    interactions_list = list()
+    variation_data = pd.DataFrame(columns=['userId', 'click_per_userId', 'queryId', 'click_per_query',
+                                           'click_per_model_A'])
+
+    variation_data_frame = primary_data.copy()
+    variation_data_frame.drop(columns=['click_per_query'], inplace=True)
+    variation_data_frame = pd.merge(variation_data_frame, click_per_query_adore, left_on='queryId', right_index=True, how='inner')
+
+    while click_per_query_adore['click_per_query'].sum > 0:
+        c = 1
+        #click_per_query_adore['selectedUserId'] = //se click_per_query_adore['click_per_query']>0 : scelgo un userId a caso else 0
+
+    for index in click_per_query_adore.index:
+        click_sum = 0
+        total_click_per_query = click_per_query_adore.loc[index, 'click_per_query']
+        while click_sum < total_click_per_query:
+            partial_data = primary_data[primary_data.queryId == click_per_query_adore.loc[index, 'queryId']]
+            interaction_index = np.random.choice(partial_data.index.values, 1)
+            interaction = partial_data.loc[interaction_index]
+            interaction['click_per_query'] = total_click_per_query
+            interactions_list.append(interaction)
+
+            click_sum = click_sum + int(interaction['click_per_userId'])
+
+        if click_sum > total_click_per_query:
+            last_elem = interactions_list[-1]
+            click_sum = int(click_sum - last_elem['click_per_userId'])
+            last_elem['click_per_userId'] = last_elem['click_per_query'] - click_sum
+            if int(last_elem['click_per_model_A']) > int(last_elem['click_per_userId']):
+                last_elem['click_per_model_A'] = np.random.randint(0, last_elem['click_per_userId'] + 1)
+            del interactions_list[-1]
+            interactions_list.append(last_elem)
+        variation_data = variation_data.append(interactions_list)
+        interactions_list.clear()
+
+    variation_data.reset_index(drop=True, inplace=True)
+
+    print('Nan stats:')
+    print(variation_data.isnull().sum())
+
+    variation_data = variation_data.astype('int32')
+
+    return variation_data
+
+
+if __name__ == '__main__':
+    # h = hpy()
+    percentage_dropped_queries = []
+
+    print('------------- Creating adore dataset ----------------')
+    start = time.time()
+    adore_dataset = utils.create_adore_dataset()
+    end = time.time()
+    print('Time: ' + str(end - start))
+    print('------------- Elaborating adore dataset -------------')
+    start = time.time()
+    adore_dataset_for_score = utils.elaborate_dataset_for_score(adore_dataset)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    print('------------- Pruning on adore dataset --------------')
+    start = time.time()
+    adore_dataset_with_pruning = utils.pruning(adore_dataset_for_score, percentage_dropped_queries)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    # print('ADORE AFTER PRUNING')
+    # print(h.heap())
+
+    print('\n------------- Creating primary dataset --------------')
+    start = time.time()
+    primary_dataset = create_primary_dataset(adore_dataset)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    print('------------- Elaborating primary dataset --------------')
+    start = time.time()
+    primary_dataset_for_score = utils.elaborate_dataset_for_score(primary_dataset)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    print('------------- Pruning on primary dataset --------------')
+    start = time.time()
+    primary_dataset_with_pruning = utils.pruning(primary_dataset_for_score, percentage_dropped_queries)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    # print('PRIMARY AFTER PRUNING')
+    # print(h.heap())
+
+    print('\n------------- Computing AB score for primary dataset --------------')
+    start = time.time()
+    ab_score_primary_no_pruning = utils.computing_ab_score(primary_dataset_for_score)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    start = time.time()
+    ab_score_primary_with_pruning = utils.computing_ab_score(primary_dataset_with_pruning)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    print('AB score without pruning for primary dataset = ' + str(ab_score_primary_no_pruning))
+    print('AB score with pruning for primary dataset = ' + str(ab_score_primary_with_pruning))
+    # print('PRIMARY AFTER AB')
+    # print(h.heap())
+
+    print('------------- Computing AB score for adore dataset --------------')
+    start = time.time()
+    ab_score_adore_no_pruning = utils.computing_ab_score(adore_dataset_for_score)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    start = time.time()
+    ab_score_adore_with_pruning = utils.computing_ab_score(adore_dataset_with_pruning)
+    end = time.time()
+    print('Time: ' + str(end - start))
+    print('AB score without pruning for adore dataset = ' + str(ab_score_adore_no_pruning))
+    print('AB score with pruning for adore dataset = ' + str(ab_score_adore_with_pruning))
+    # print('ADORE AFTER AB')
+    # print(h.heap())
+
+    agree_no_pruning = 0
+    agree_with_pruning = 0
+    agree_between_variation = 0
+    adore_total_click_for_variation = adore_dataset.drop_duplicates(
+        subset=['queryId', 'click_per_query'], keep='last')[['queryId', 'click_per_query']]
+    adore_total_click_for_variation.set_index('queryId', inplace=True)
+    del adore_dataset
+    del adore_dataset_for_score
+    del adore_dataset_with_pruning
+    del primary_dataset_for_score
+    del primary_dataset_with_pruning
+    # print('AFTER DEL')
+    # print(h.heap())
+
+    for i in range(0, 100):
+        print('\n\n***************************** Round ' + str(i) + ' ************************************')
+        print('------------- Creating variation dataset --------------')
+        start = time.time()
+        variation_dataset = create_variation_dataset(primary_dataset, adore_total_click_for_variation)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        print('------------- Elaborating variation dataset --------------')
+        start = time.time()
+        variation_dataset_for_score = utils.elaborate_dataset_for_score(variation_dataset)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        # print('BEFORE VAR DEL')
+        # print(h.heap())
+        del variation_dataset
+        # print('AFTER VAR DEL')
+        # print(h.heap())
+        print('------------- Pruning variation dataset --------------')
+        start = time.time()
+        variation_dataset_with_pruning = utils.pruning(variation_dataset_for_score, percentage_dropped_queries)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        # print('AFTER VAR PRUNING')
+        # print(h.heap())
+
+        print('\n------------- Computing AB score for variation dataset --------------')
+        start = time.time()
+        ab_score_variation_no_pruning = utils.computing_ab_score(variation_dataset_for_score)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        start = time.time()
+        ab_score_variation_with_pruning = utils.computing_ab_score(variation_dataset_with_pruning)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        print('AB score without pruning for variation dataset = ' + str(ab_score_variation_no_pruning))
+        print('AB score with pruning for variation dataset = ' + str(ab_score_variation_with_pruning))
+        # print('BEFORE VAR AB')
+        # print(h.heap())
+        del variation_dataset_for_score
+        del variation_dataset_with_pruning
+        # print('AFTER VAR DEL')
+        # print(h.heap())
+
+        print('------------- Comparing AB score between variation dataset and primary dataset --------------')
+        start = time.time()
+        comparison_with_primary = utils.same_score(ab_score_primary_no_pruning, ab_score_variation_no_pruning)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        # print('AFTER COMPARISON')
+        # print(h.heap())
+        if comparison_with_primary:
+            agree_no_pruning = agree_no_pruning + 1
+
+        start = time.time()
+        comparison_with_primary_pruning = utils.same_score(ab_score_primary_with_pruning,
+                                                           ab_score_variation_with_pruning)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        # print('AFTER COMPARISON')
+        # print(h.heap())
+        if comparison_with_primary_pruning:
+            agree_with_pruning = agree_with_pruning + 1
+
+        start = time.time()
+        comparison_between_variation = utils.same_score(ab_score_variation_no_pruning, ab_score_variation_with_pruning)
+        end = time.time()
+        print('Time: ' + str(end - start))
+        # print('AFTER COMPARISON')
+        # print(h.heap())
+        if comparison_between_variation:
+            agree_between_variation = agree_between_variation + 1
+
+    print('Number of times the TDI is correct = ' + str(agree_no_pruning) + '/100')
+    print('Number of times the SS is correct = ' + str(agree_with_pruning) + '/100')
+    print('Average percentage of dropped queries = ' + str(sum(percentage_dropped_queries) / len(
+        percentage_dropped_queries)))
+    # print('Number of times the variation with and without pruning agree = ' + str(agree_between_variation) + '/100')
---
+++ utils.py
@@ -0,0 +1,170 @@
+import sys
+import scipy.stats
+import numpy as np
+import pandas as pd
+
+
+def create_adore_dataset():
+    print('Reading json file')
+    raw_data = pd.read_json('./dataset_from_adore/query_click_user.json')
+    raw_data = pd.json_normalize(raw_data['parent_buckets'], record_path=['users'], meta=['val', 'count'],
+                                 meta_prefix='query_')
+    raw_data.rename(columns={'val': 'userId', 'count': 'click_per_userId', 'query_val': 'queryId',
+                             'query_count': 'click_per_query'}, inplace=True)
+    raw_data['userId'] = raw_data.groupby('queryId').userId.cumcount() + 1
+
+    raw_data = raw_data.astype({'queryId': 'int64', 'click_per_query': 'int64', 'userId': 'int16'})
+
+    # SOLO PER TEST
+    # raw_data = raw_data.head(3000)
+
+    print('Fixing click per query')
+    sum_clicks = pd.DataFrame(raw_data.groupby('queryId')['click_per_userId'].sum()).reset_index()
+    sum_clicks.rename(columns={'click_per_userId': 'click_per_query'}, inplace=True)
+    raw_data = pd.merge(raw_data, sum_clicks, how='left', on='queryId')
+    raw_data.drop(columns='click_per_query_x', inplace=True)
+    raw_data.rename(columns={'click_per_query_y': 'click_per_query'}, inplace=True)
+
+    print('Populating click_per_model_A')
+    clicks_list = list()
+    for index in raw_data.index:
+        min_data = 0
+        max_data = raw_data['click_per_userId'][index]
+        clicks = np.random.randint(min_data, max_data + 1)
+        clicks_list.append(int(clicks))
+    click_per_model_a = pd.Series(clicks_list, index=raw_data.index)
+    raw_data['click_per_model_A'] = click_per_model_a
+
+    print('Nan stats:')
+    print(raw_data.isnull().sum())
+
+    return raw_data
+
+
+def elaborate_dataset_for_score(interleaving_dataset):
+    print('Computing total_click_per_model_A')
+    total_click_per_model_a = pd.DataFrame(interleaving_dataset.groupby(['queryId'])['click_per_model_A'].sum())
+    total_click_per_model_a.reset_index(inplace=True)
+    total_click_per_model_a.rename(columns={'click_per_model_A': 'total_click_per_model_A'}, inplace=True)
+    interleaving_dataset = pd.merge(interleaving_dataset, total_click_per_model_a, on='queryId', how='left')
+    interleaving_dataset.drop_duplicates(subset=['queryId', 'click_per_query', 'total_click_per_model_A'],
+                                         keep='last', inplace=True)
+    interleaving_dataset.drop(columns=['userId', 'click_per_userId', 'click_per_model_A'], inplace=True)
+
+    print('Computing winning model')
+    interleaving_dataset['winning_model'] = 2
+    interleaving_dataset.loc[interleaving_dataset[
+                                 'total_click_per_model_A'] > interleaving_dataset['click_per_query'] / 2,
+                             'winning_model'] = 0
+    interleaving_dataset.loc[interleaving_dataset[
+                                 'total_click_per_model_A'] < interleaving_dataset['click_per_query'] / 2,
+                             'winning_model'] = 1
+
+    interleaving_dataset.loc[
+        interleaving_dataset['winning_model'] == 1,
+        'total_click_per_model_A'] = interleaving_dataset['click_per_query'] - interleaving_dataset[
+        'total_click_per_model_A']
+    interleaving_dataset.rename(columns={'total_click_per_model_A': 'click_per_winning_model'}, inplace=True)
+
+    print('Nan stats:')
+    print(interleaving_dataset.isnull().sum())
+
+    return interleaving_dataset
+
+
+def generate_new_data(data_to_add_stats, user_id_max):
+    print('Generating random click_per_userId for primary dataset')
+    clicks_list = list()
+    new_data = pd.DataFrame(columns=['queryId', 'click_per_userId'])
+    for index in data_to_add_stats.index:
+        if data_to_add_stats.loc[index, 'new_interactions_to_add'] > 0:
+            # max_clicks = data_to_add_stats.loc[index, 'new_interactions_to_add'] + 1
+            while sum(clicks_list) < data_to_add_stats.loc[index, 'new_interactions_to_add']:
+                clicks = np.random.randint(1, data_to_add_stats.loc[index, 'new_interactions_to_add'] + 1)
+                clicks_list.append(clicks)
+                # max_clicks = max_clicks - clicks + 1
+            del clicks_list[-1]
+            clicks_list.append(data_to_add_stats.loc[index, 'new_interactions_to_add'] - sum(clicks_list))
+            data_to_append = {'queryId': [data_to_add_stats.loc[index, 'queryId']] * len(clicks_list),
+                              'click_per_userId': clicks_list,
+                              'click_per_query': [data_to_add_stats.loc[index, 'click_per_query']] * len(clicks_list)}
+            new_data = new_data.append(pd.DataFrame(data_to_append))
+            clicks_list.clear()
+
+    new_data['userId'] = pd.Series(np.random.randint(0, user_id_max, size=new_data.shape[0]))
+
+    new_data.reset_index(drop=True, inplace=True)
+    print('Populating click_per_model_A')
+    clicks_per_user_list = list()
+    for index in new_data.index:
+        min_data = 0
+        max_data = new_data['click_per_userId'][index]
+        clicks = np.random.randint(min_data, max_data + 1)
+        clicks_per_user_list.append(int(clicks))
+    click_per_model_a = pd.Series(clicks_per_user_list, index=new_data.index)
+    new_data['click_per_model_A'] = click_per_model_a
+
+    return new_data
+
+
+def statistical_significance_computation(interactions, overall_diff):
+    p = overall_diff
+    interactions['cumulative_distribution_left'] = scipy.stats.binom.cdf(interactions.click_per_winning_model,
+                                                                         interactions.click_per_query, p)
+    interactions['pmf'] = scipy.stats.binom.pmf(interactions.click_per_winning_model, interactions.click_per_query, p)
+    interactions['cumulative_distribution_right'] = 1 - interactions[
+        'cumulative_distribution_left'] + interactions['pmf']
+    interactions['statistical_significance'] = 2 * interactions[[
+        'cumulative_distribution_left', 'cumulative_distribution_right']].min(axis=1) + sys.float_info.epsilon
+    interactions.drop(columns=['cumulative_distribution_left', 'cumulative_distribution_right', 'pmf'], inplace=True)
+    # print('AFTER STATISTICAL SIGNIFICANCE')
+    # print(h.heap())
+    return interactions
+
+
+def pruning(interleaving_dataset, percentage_dropped_queries):
+    overall_diff = 0.5
+    # print('PRUNING')
+    # print(h.heap())
+    per_query_model_interactions = statistical_significance_computation(interleaving_dataset.copy(), overall_diff)
+    # Remove interactions with significance higher than 5% threshold
+    queries_before_drop = per_query_model_interactions.shape[0]
+    print('Number of queries before drop: ' + str(queries_before_drop))
+    per_query_model_interactions = per_query_model_interactions[
+        per_query_model_interactions.statistical_significance < 0.05]
+    queries_after_drop = per_query_model_interactions.shape[0]
+    print('Number of queries after drop: ' + str(queries_after_drop))
+    per_query_model_interactions = per_query_model_interactions.drop(columns='statistical_significance')
+
+    print('Dropped queries: ' + str(queries_before_drop - queries_after_drop))
+    percentage = (queries_before_drop - queries_after_drop) * 100 / queries_before_drop
+    percentage_dropped_queries.append(percentage)
+    print('Percentage dropped queries: ' + str(percentage))
+
+    print('Nan stats:')
+    print(per_query_model_interactions.isnull().sum())
+
+    return per_query_model_interactions
+
+
+def computing_ab_score(interleaving_dataset):
+    winner_a = len(interleaving_dataset[interleaving_dataset['winning_model'] == 0])
+    winner_b = len(interleaving_dataset[interleaving_dataset['winning_model'] == 1])
+    ties = len(interleaving_dataset[interleaving_dataset['winning_model'] == 2])
+
+    # Delta score
+    delta_ab = (winner_a + 1 / 2 * ties) / (winner_a + winner_b + ties) - 0.5
+
+    print('Nan stats:')
+    print(interleaving_dataset.isnull().sum())
+
+    return round(delta_ab, 3)
+
+
+def same_score(ab_first, ab_second):
+    if (ab_first > 0 and ab_second > 0) or (
+            ab_first == 0 and ab_second == 0) or (
+            ab_first < 0 and ab_second < 0):
+        return True
+    else:
+        return False
